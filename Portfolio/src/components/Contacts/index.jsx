48b1a7c67acbc84723857e1fd173891ce0e4b5f7
gabizinha <gabrielamiyazaki55@gmail.com>
2024-03-21 17:40:50 -0300

    Installation of icon library and creation of contacts page.

---
+++ Portfolio/src/components/Contacts/index.jsx
@@ -0,0 +1,13 @@
+
+export const Contacts = () => {
+    return(
+        <div>
+            <div>
+                <h2>icon</h2>
+                <h2>icon</h2>
+                <h2>icon</h2>
+                <h2>icon</h2>
+            </div>
+        </div>
+    )
+}
