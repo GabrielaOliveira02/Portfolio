ecf6d88c3b0e06c96dce0b1bef91135b3fa72351
gabizinha <gabrielamiyazaki55@gmail.com>
2024-03-21 17:37:08 -0300

    creation of the header and footer

---
+++ Portfolio/src/components/Footer/index.jsx
@@ -0,0 +1,8 @@
+
+export const Footer = () => {
+    return(
+            <div>
+                <h2>Desenvolvido por Gabriela Oliveira #icon de gato aqui</h2>
+            </div>
+    )
+}
---
+++ Portfolio/src/components/Header/index.jsx
@@ -0,0 +1,14 @@
+
+export const Header = () => {
+    return(
+        <div>
+            <div>
+                <h2>Inicio</h2>
+                <h2>Sobre</h2>
+                <h2>Skills</h2>
+                <h2>Projetos</h2>
+                <h2>Contato</h2>
+            </div>
+        </div>
+    )
+}
